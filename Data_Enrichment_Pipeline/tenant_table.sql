585b12ea70abe7810f6ae92ee6b889de977d72e2
Ayush Agrawal <130290786+AyushAgrawal31@users.noreply.github.com>
2025-07-05 04:13:50 +0530

    Create tenant_table.sql

---
+++ Data_Enrichment_Pipeline/tenant_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [Warehouse].[Gold].[Tenant]
+(
+	[Tenant] [varchar](255) NULL,
+	[TenantName] [varchar](255) NULL
+)
+GO
+INSERT into Gold.Tenant VALUES ('TenantA','A'),('TenantB','B');
